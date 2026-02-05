d7bb05c980436a24c189b48444969c90acd8c884
Duke <emersonalmeidax@gmail.com>
2026-02-05 17:33:35 -0300

    initial kafka-consumer

---
+++ {{cookiecutter.project_slug}}/driver/kafka-consumer/src/main/kotlin/br/com/caju/Application.kt
@@ -0,0 +1,13 @@
+package br.com.caju
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@ConfigurationPropertiesScan("br.com.caju")
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ {{cookiecutter.project_slug}}/driver/kafka-consumer/src/main/kotlin/br/com/caju/shared/config/KafkaConstants.kt
@@ -0,0 +1,6 @@
+package br.com.caju.shared.config
+
+object KafkaConstants {
+    const val OFFSET_RESET_EARLIEST = "auto.offset.reset=earliest"
+    const val KAFKA_OPERATION_NAME = "kafka.consumer"
+}
